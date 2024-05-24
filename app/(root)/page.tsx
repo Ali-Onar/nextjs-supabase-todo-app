af6722ac96aab61fba76c21d55a884728bfb468b
Ali Onar <ali.onar@seamlesscare.ie>
2024-05-25 00:10:20 +0300

    refactor: Update technologies list and layout on homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,42 +4,31 @@ import { Typography, List, ListItem, ListItemText, Grid, Container } from '@mui/
 const technologies = [
     { name: 'Next.js 14 App Router', description: 'Modern front-end routing.' },
     { name: 'Supabase for DB', description: 'Fast and reliable database management with PostgreSQL.' },
+    { name: 'RLS in Supabase', description: 'Row-level security for data protection.' },
     { name: 'Clerk for Auth', description: 'User authentication and management.' },
-    { name: 'Next.js Route Handler', description: 'Effective routing for server-side and API path operations.' },
     { name: 'Material UI', description: 'Visual and user-friendly interface components.' },
     { name: 'CRUD Operations', description: 'Create, Read, Update, Delete operations.' },
     { name: 'TypeScript', description: 'JavaScript development with strong type checking.' },
     { name: '@dnd-kit/core', description: 'Comprehensive tools for drag and drop functionality.' },
-];
-
-const pagesDescription = [
-    { title: 'Basic Todos', description: 'Client Side Rendered. Route Handler is used for CRUD operations.' },
-    { title: 'D&D Todos', description: 'Client Side Rendered. The createClerkSupabaseClient function is used for CRUD operations.' },
-    { title: 'Server Side Test', description: 'Server Side Rendered. getSupabaseServer function is used.' },
+    { name: 'Sentry for Error Tracking', description: 'Real-time error tracking and monitoring.' },
 ];
 
 const HomePage = () => (
     <Container maxWidth="xl">
         <Grid container spacing={2} sx={{ mt: 5 }}>
-            <Grid item xs={12} md={6}>
-                <Typography variant="h4" gutterBottom>
-                    About Pages:
-                </Typography>
-                <List>
-                    {pagesDescription.map((tech, index) => (
-                        <ListItem key={index}>
-                            <ListItemText primary={tech.title} secondary={tech.description} />
-                        </ListItem>
-                    ))}
-                </List>
-            </Grid>
-            <Grid item xs={12} md={6}>
+            <Grid
+                item
+                xs={12}
+                sx={{
+                    display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center',
+                }}
+            >
                 <Typography variant="h4" gutterBottom>
                     Technologies Used in Our Project
                 </Typography>
                 <List>
-                    {technologies.map((tech, index) => (
-                        <ListItem key={index}>
+                    {technologies.map((tech) => (
+                        <ListItem key={tech.name}>
                             <ListItemText primary={tech.name} secondary={tech.description} />
                         </ListItem>
                     ))}
